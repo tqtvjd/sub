9c8cf84d177d6d7cafd8c3ea5f61b61b3a99dcd4
Ariel <107245128+arielherself@users.noreply.github.com>
2022-07-11 23:17:05 +0800

    Update autosub_v2b.py

--- autosub_v2b.py
+++ autosub_v2b.py
@@ -7,11 +7,11 @@ V2B_REG_REL_URL = '/api/v1/passport/auth/register'
 # V2B_SUB_REL_URL = '/api/v1/user/getSubscribe'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0'
 
-home_urls = [
+home_urls = (
     'https://sub.chbjpw.mobi', 
     'https://cooc.cloud',
     'https://xf.gl'
-]
+)
 times = 15
 proxies = {
     'http': 'http://127.0.0.1:7890',
